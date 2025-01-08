0ffe55a0e46d2b6c67b1bdc5ae9880a3e000b201
Hadi Behgam <h.behgam@gmail.com>
2025-01-08 17:06:46 +0330

    Create page.tsx

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignupTemplate from '@/components/template/auth/signup'
+import React from 'react'
+
+function signupPage() {
+  return (
+    <SignupTemplate />
+  )
+}
+
+export default signupPage
