d1244b1ae75399919d9d1fedc9c9a75797d1172a
Hadi Behgam <h.behgam@gmail.com>
2025-01-08 17:06:52 +0330

    Create page.tsx

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import SigninTemplate from '@/components/template/auth/signin'
+import React from 'react'
+
+function signinPage() {
+  return (
+    <SigninTemplate />
+  )
+}
+
+export default signinPage
