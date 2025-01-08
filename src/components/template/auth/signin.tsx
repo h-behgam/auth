13bef656701d8a9f3001864b3006ab8d0ea9e5b4
Hadi Behgam <h.behgam@gmail.com>
2025-01-08 14:23:11 +0330

    update

---
+++ src/components/template/auth/signin.tsx
@@ -0,0 +1,13 @@
+
+function signin() {
+  return (
+    <div className="max-w-3xl">
+        <form className="">
+            <div className="p-1 mb-2"></div>
+            <div className="p-1 mb-2"></div>
+        </form>
+    </div>
+  )
+}
+
+export default signin
