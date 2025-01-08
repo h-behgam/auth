f8253be6ae2ba71f8e3f4a64fdb45538d223172c
Hadi Behgam <h.behgam@gmail.com>
2025-01-08 17:06:36 +0330

    Update signin.tsx

--- src/components/template/auth/signin.tsx
+++ src/components/template/auth/signin.tsx
@@ -1,13 +1,50 @@
+import ClientButton from '@/components/global/client-button';
+import CustomInput from '@/components/global/custom-input';
+import Link from 'next/link';
 
-function signin() {
+function SigninTemplate() {
   return (
-    <div className="max-w-3xl">
-        <form className="">
-            <div className="p-1 mb-2"></div>
-            <div className="p-1 mb-2"></div>
-        </form>
+    <div className='mx-auto w-full max-w-md'>
+      <div className='mb-6 text-center'>
+        <h2 className='text-3xl font-bold'>Login</h2>
+        <p>Enter your email below to login to your account</p>
+      </div>
+      <form className=''>
+        <div className='mb-2 p-1'>
+          <CustomInput
+            name='username'
+            type='text'
+            placeholder='Username'
+            labalName='username'
+            labelTitle='user'
+          />
+        </div>
+        <div className='mb-2 p-1'>
+          <CustomInput
+            name='password'
+            type='text'
+            placeholder='Password'
+            labalName='password'
+            labelTitle='Password'
+          />
+        </div>
+        <div className='mb-2 text-right'>
+          <Link
+            href={'/forgot-password'}
+            className='text-sm text-indigo-700 hover:text-indigo-600'
+          >
+            Forgot your password?
+          </Link>
+        </div>
+        <div>
+          <ClientButton disabled={false}>Sign in</ClientButton>
+        </div>
+      </form>
+      <p className='mt-4 text-center'>
+        Don't have an account? <Link href={'/signup'}>Sign up</Link>
+      </p>
     </div>
-  )
+  );
 }
 
-export default signin
+export default SigninTemplate;
