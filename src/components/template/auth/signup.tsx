13e19ac5be27c82cee97941b5815e5c08a535f74
Hadi Behgam <h.behgam@gmail.com>
2025-01-08 17:06:34 +0330

    Create signup.tsx

---
+++ src/components/template/auth/signup.tsx
@@ -0,0 +1,51 @@
+import ClientButton from '@/components/global/client-button';
+import CustomInput from '@/components/global/custom-input';
+import Link from 'next/link';
+
+function SignupTemplate() {
+  return (
+    <div className='mx-auto w-full max-w-md'>
+      <div className='mb-6 text-center'>
+        <h2 className='text-3xl font-bold'>Create an account</h2>
+        <p>Enter your information to get started</p>
+      </div>
+      <form className=''>
+        <div className='mb-2 p-1'>
+          <CustomInput
+            name='username'
+            type='text'
+            placeholder='Username'
+            labalName='username'
+            labelTitle='username'
+          />
+        </div>
+        <div className='mb-2 p-1'>
+          <CustomInput
+            name='password'
+            type='text'
+            placeholder='Password'
+            labalName='password'
+            labelTitle='Password'
+          />
+        </div>
+        <div className='mb-2 p-1'>
+          <CustomInput
+            name='password'
+            type='text'
+            placeholder='Retype Password'
+            labalName='password'
+            labelTitle='Retype Password'
+          />
+        </div>
+        <div>
+          <ClientButton disabled={false}>Sign up</ClientButton>
+        </div>
+      </form>
+      <p className='text-center mt-4'>
+        Already have an account? <Link href={'/login'}>Login</Link>{' '}
+      </p>
+    </div>
+  );
+}
+
+export default SignupTemplate;
