327f922a9535915851b7e4d9f96a9b359ae8ec5d
Hadi Behgam <h.behgam@gmail.com>
2025-01-20 18:52:48 +0330

    Update next-auth.ts

--- src/lib/next-auth.ts
+++ src/lib/next-auth.ts
@@ -1,16 +1,31 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import NextAuth from 'next-auth';
+import NextAuth, { AuthError } from 'next-auth';
 import PrismaDB from './prisma';
 import Credentials from 'next-auth/providers/credentials';
 import { formSchema } from './zod';
 import { ZodError } from 'zod';
 import { compare } from 'bcrypt';
+import {
+  customError,
+  CustomCredentialsSignin,
+  logger,
+  PasswordInccorectError,
+  UserDoesNotExistError,
+  CustomErrorr,
+} from './custom-error';
 // import authConfig from './auth.config';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   // ...authConfig,
   adapter: PrismaAdapter(PrismaDB),
+  debug: false,
   session: { strategy: 'jwt' },
+  pages: {
+    signIn: '/auth/sign-in',
+    verifyRequest: '/auth/auth-success',
+    error: '/auth/auth-error',
+  },
+  logger: logger,
   providers: [
     Credentials({
       credentials: {
@@ -25,33 +40,72 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
       authorize: async (credentials): Promise<any> => {
         try {
-          const validCredentials = formSchema.safeParse(credentials);
-          if (!validCredentials.success) {
-            return { error: validCredentials.error.flatten().fieldErrors };
-          }
-          const { username, password } = validCredentials.data;
+          const { username, password } =
+            await formSchema.parseAsync(credentials);
+          console.log('continu auth');
+
+          // const { username, password } = validCredentials.data;
           const user = await PrismaDB.user.findUnique({ where: { username } });
 
           if (!user) {
-            return null;
+            throw new UserDoesNotExistError();
           }
+
           const isPasswordValid = await compare(
             password,
             user?.password as string,
           );
+
           if (!isPasswordValid) {
-            return null;
+            throw new PasswordInccorectError();
+            throw new CustomCredentialsSignin(
+              'The password is not corect!',
+              'wrongPassword',
+            );
           }
           return user;
-        } catch (error) {
-          if (error instanceof ZodError) {
-            console.log('error', error.errors);
+        } catch (error: any) {
+          if (
+            error instanceof UserDoesNotExistError ||
+            error instanceof PasswordInccorectError 
+          ) {
+            throw error;
+          } else if (error instanceof ZodError) {
 
-            // Return `null` to indicate that the credentials are invalid
-            return null;
+
+            const a = error.flatten().fieldErrors
+            console.log(
+              'error in next auth catch',
+              a,
+            );
+            const err = error.flatten().fieldErrors;
+            // console.log('errerrerrerr', err);
+            throw new CustomCredentialsSignin(JSON.stringify(error.flatten().fieldErrors));
+          } else {
+            throw new Error('Unexpected error occurred during authorization');
           }
+
+          if (error instanceof ZodError) {
+            console.log(
+              'error in next auth catch',
+              error.flatten().fieldErrors,
+            );
+            const err = error.flatten().fieldErrors;
+            console.log(err);
+            throw new CustomCredentialsSignin(err, 'zoderror');
+          }
+
+          if (error.type == 'wrongPassword') {
+            // console.log({error});
+            throw new CustomCredentialsSignin(
+              error.message,
+              'wrongPassword',
+              'passwordError',
+            );
+          }
+
+          return null;
         }
-        return null;
       },
     }),
   ],
