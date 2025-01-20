7d497a2097bfff342cd89d09f779c62b968f9ec8
Hadi Behgam <h.behgam@gmail.com>
2025-01-20 18:53:04 +0330

    Update signin.tsx

--- src/components/template/auth/signin.tsx
+++ src/components/template/auth/signin.tsx
@@ -1,27 +1,68 @@
+'use client'
 import ClientButton from '@/components/global/client-button';
 import CustomInput from '@/components/global/custom-input';
 import { signIn } from '@/lib/next-auth';
 import Link from 'next/link';
+import { login } from '../../../../actions/auth';
+import { useActionState } from 'react';
+// import { signIn } from 'next-auth/react';
+export type FormState =
+  | {
+      errors?: {
+        name?: string[];
+        email?: string[];
+        password?: string[];
+      };
+      message?: string;
+    }
+  | undefined;
 
 function SigninTemplate() {
-  const handlerSubmit = (formData: FormData) => {
-    signIn('credentials', {formData, callbackUrl: '/dashboard'});
-  }
+  // const handlerSubmit = (
+  //   state: FormState,
+  //   formData: FormData,
+  // ) => {
+  //   signIn('credentials', { formData, callbackUrl: '/dashboard' });
+  // };
+  const [state, action, pending] = useActionState(login, {
+    message: "",
+    error: null,
+  });
+  console.log('state', state);
+  
   return (
     <div className='mx-auto w-full max-w-md'>
       <div className='mb-6 text-center'>
         <h2 className='text-3xl font-bold'>Login</h2>
         <p>Enter your email below to login to your account</p>
       </div>
-      <form className='' action={async (formData) => {
-        "use server"
-        const { username, password } = Object.fromEntries(formData)
-        console.log(username, password );
-        
-        const a = await signIn("credentials", {username, password, callbackUrl: "/dashboard"})
-        console.log('aaaaaaaaaaaa',a);
-        
-      }}>
+      <form
+        className=''
+        action={action
+        //   async (formData) => {
+        //   'use server';
+        //   console.log(11111);
+          
+        //   try {
+            
+          
+        //   const { username, password } = Object.fromEntries(formData);
+        //   console.log(username, password);
+
+        //   const a = await signIn('credentials', {
+        //     username,
+        //     password,
+        //     redirect: false,
+        //     // callbackUrl: '/dashboard',
+        //   });
+        //   console.log('aaaaaaaaaaaa', a);
+        // } catch (error) {
+        //     console.log('errorrrrrrrr', {error});
+            
+        // }
+        // }
+      }
+      >
         <div className='mb-2 p-1'>
           <CustomInput
             name='username'
