b5b4c8f7aa745dbd5918779a88a5f7ac4cf9938a
Hadi Behgam <h.behgam@gmail.com>
2025-01-20 18:52:53 +0330

    Update auth.config.ts

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,7 +1,6 @@
-import type { NextAuthConfig } from "next-auth"
-import Credentials from "next-auth/providers/credentials"
- 
+import type { NextAuthConfig } from 'next-auth';
+
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [Credentials],
-} satisfies NextAuthConfig
+  providers: [],
+} satisfies NextAuthConfig;
