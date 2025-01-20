49605e54e5f9955bbbb192f1b5d152ab14733b84
Hadi Behgam <h.behgam@gmail.com>
2025-01-20 18:52:44 +0330

    Update zod.ts

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -1,6 +1,8 @@
 import { object, string } from 'zod';
 
 export const formSchema = object({
-  username: string({ required_error: 'username is requierd' }).min(8).max(20),
-  password: string().min(8).max(20),
+  username: string({ required_error: 'username is requierd' }).min(1).max(20),
+  password: string().min(2).max(20)
+  // .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+  // .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
 });
