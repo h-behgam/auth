762c44ce83a27fd78ba026d881676148380fc78f
Hadi Behgam <h.behgam@gmail.com>
2025-01-21 16:57:44 +0330

    Delete

--- src/lib/zod.ts
+++
@@ -1,8 +0,0 @@
-import { object, string } from 'zod';
-
-export const formSchema = object({
-  username: string({ required_error: 'username is requierd' }).min(1).max(20),
-  password: string().min(2).max(20)
-  // .regex(/[a-z]/, "Password must contain at least one lowercase letter")
-  // .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-});
