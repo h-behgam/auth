a9b49d73978153d7a4856ec7419d93a819363d56
Hadi Behgam <h.behgam@gmail.com>
2025-01-21 13:31:24 +0330

    Update next-auth.ts

--- src/lib/next-auth.ts
+++ src/lib/next-auth.ts
@@ -1,31 +1,31 @@
+import NextAuth from 'next-auth';
+import authConfig from './auth.config';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import NextAuth, { AuthError } from 'next-auth';
-import PrismaDB from './prisma';
 import Credentials from 'next-auth/providers/credentials';
+
+import PrismaDB from './prisma';
+
 import { formSchema } from './zod';
 import { ZodError } from 'zod';
+
 import { compare } from 'bcrypt';
+
 import {
-  customError,
   CustomCredentialsSignin,
   logger,
   PasswordInccorectError,
   UserDoesNotExistError,
-  CustomErrorr,
-} from './custom-error';
-// import authConfig from './auth.config';
+} from './custom-auth-error';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  // ...authConfig,
+  ...authConfig,
   adapter: PrismaAdapter(PrismaDB),
   debug: false,
   session: { strategy: 'jwt' },
-  pages: {
-    signIn: '/auth/sign-in',
-    verifyRequest: '/auth/auth-success',
-    error: '/auth/auth-error',
-  },
   logger: logger,
+  pages: {
+    signIn: '/login',
+  },
   providers: [
     Credentials({
       credentials: {
@@ -42,69 +42,30 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         try {
           const { username, password } =
             await formSchema.parseAsync(credentials);
-          console.log('continu auth');
-
-          // const { username, password } = validCredentials.data;
           const user = await PrismaDB.user.findUnique({ where: { username } });
 
-          if (!user) {
-            throw new UserDoesNotExistError();
-          }
+          if (!user) throw new UserDoesNotExistError();
 
           const isPasswordValid = await compare(
             password,
             user?.password as string,
           );
 
-          if (!isPasswordValid) {
-            throw new PasswordInccorectError();
-            throw new CustomCredentialsSignin(
-              'The password is not corect!',
-              'wrongPassword',
-            );
-          }
+          if (!isPasswordValid) throw new PasswordInccorectError();
           return user;
         } catch (error: any) {
           if (
             error instanceof UserDoesNotExistError ||
-            error instanceof PasswordInccorectError 
+            error instanceof PasswordInccorectError
           ) {
             throw error;
           } else if (error instanceof ZodError) {
-
-
-            const a = error.flatten().fieldErrors
-            console.log(
-              'error in next auth catch',
-              a,
+            throw new CustomCredentialsSignin(
+              JSON.stringify(error.flatten().fieldErrors),
             );
-            const err = error.flatten().fieldErrors;
-            // console.log('errerrerrerr', err);
-            throw new CustomCredentialsSignin(JSON.stringify(error.flatten().fieldErrors));
           } else {
             throw new Error('Unexpected error occurred during authorization');
           }
-
-          if (error instanceof ZodError) {
-            console.log(
-              'error in next auth catch',
-              error.flatten().fieldErrors,
-            );
-            const err = error.flatten().fieldErrors;
-            console.log(err);
-            throw new CustomCredentialsSignin(err, 'zoderror');
-          }
-
-          if (error.type == 'wrongPassword') {
-            // console.log({error});
-            throw new CustomCredentialsSignin(
-              error.message,
-              'wrongPassword',
-              'passwordError',
-            );
-          }
-
-          return null;
         }
       },
     }),
