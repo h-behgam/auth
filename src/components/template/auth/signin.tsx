21ae74a77a6d51e3359287dbc7b5581425b7769c
Hadi Behgam <h.behgam@gmail.com>
2025-01-21 16:57:48 +0330

    Update signin.tsx

--- src/components/template/auth/signin.tsx
+++ src/components/template/auth/signin.tsx
@@ -1,12 +1,15 @@
-'use client'
+'use client';
 import ClientButton from '@/components/global/client-button';
 import CustomInput from '@/components/global/custom-input';
 import Link from 'next/link';
 import { login } from '../../../../actions/auth';
 import { useActionState } from 'react';
 // import { signIn } from 'next-auth/react';
-
-
+type initialFormState = {
+  error?: any | undefined;
+  message?: any | undefined;
+};
+const initialState: initialFormState = { message: '', error: null };
 function SigninTemplate() {
   // const handlerSubmit = (
   //   state: FormState,
@@ -14,44 +17,17 @@ function SigninTemplate() {
   // ) => {
   //   signIn('credentials', { formData, callbackUrl: '/dashboard' });
   // };
-  const [state, action, pending] = useActionState( login, { message: '', error: null }
-
-  );
+  const [state, action, pending] = useActionState(login, initialState);
   console.log('state', state);
-  
+  console.log('pending', pending);
+
   return (
     <div className='mx-auto w-full max-w-md'>
       <div className='mb-6 text-center'>
         <h2 className='text-3xl font-bold'>Login</h2>
         <p>Enter your email below to login to your account</p>
       </div>
-      <form
-        className=''
-        action={action
-        //   async (formData) => {
-        //   'use server';
-        //   console.log(11111);
-          
-        //   try {
-            
-          
-        //   const { username, password } = Object.fromEntries(formData);
-        //   console.log(username, password);
-
-        //   const a = await signIn('credentials', {
-        //     username,
-        //     password,
-        //     redirect: false,
-        //     // callbackUrl: '/dashboard',
-        //   });
-        //   console.log('aaaaaaaaaaaa', a);
-        // } catch (error) {
-        //     console.log('errorrrrrrrr', {error});
-            
-        // }
-        // }
-      }
-      >
+      <form className='' action={action}>
         <div className='mb-2 p-1'>
           <CustomInput
             name='username'
@@ -60,7 +36,8 @@ function SigninTemplate() {
             labalName='username'
             labelTitle='user'
           />
-        </div>
+          {Object.values(state.error?.message.username).map((item:any) => <p className='text-red-700 mt-1'>{item}</p> ) }
+          </div>
         <div className='mb-2 p-1'>
           <CustomInput
             name='password'
@@ -69,6 +46,7 @@ function SigninTemplate() {
             labalName='password'
             labelTitle='Password'
           />
+          {Object.values(state.error?.message.password).map((item:any) => <p className='text-red-700 mt-1'>{item}</p> ) }
         </div>
         <div className='mb-2 text-right'>
           <Link
