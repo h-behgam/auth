903f5bd108ec1af9b9eeee01f17b240e8a65b8b8
Hadi Behgam <h.behgam@gmail.com>
2025-01-21 16:57:28 +0330

    move to auth folder

---
+++ src/lib/auth/custom-auth-error.ts
@@ -0,0 +1,59 @@
+import { AuthError, CredentialsSignin } from 'next-auth';
+
+/**
+ * This is a Custom error for auth v5
+ * @param message string | {}
+ * @param type any
+ * @param name string
+ */
+export class customError extends AuthError {
+  constructor(message: string, type: any, name?: string) {
+    super();
+    this.message = message;
+    this.type = type;
+    this.name = name || 'CustomError';
+    this.stack = undefined;
+  }
+}
+
+/**
+ * This is a Custom cridential error for auth v5
+ * @param code
+ * @param type any
+ * @param name string
+ * @param message string
+ */
+export class CustomCredentialsSignin extends CredentialsSignin {
+  constructor(message: string) {
+    super();
+    this.name = 'Error';
+    this.message = message;
+    this.code = 'AuthError';
+    this.stack = undefined;
+  }
+}
+
+// throw new error if user does not exist
+export class UserDoesNotExistError extends CredentialsSignin {
+  code = 'AuthError';
+  message = 'User does not exist - Please check credentials';
+  stack = undefined;
+}
+
+// throw new error if password is incorrect
+export class PasswordInccorectError extends CredentialsSignin {
+  code = 'AuthError';
+  message = 'Password is incorrect - Please check credentials';
+  stack = undefined;
+}
+
+// use logger to log errors
+export const logger = {
+  error: (code: any, ...message: any) => {
+    // if (code.includes('[auth][error]')) return; // فیلتر خطاهای خاص
+    // console.error('loger error is: ', {code});
+    return;
+  }, // جلوگیری از نمایش خطاها
+  // warn: console.warn, // نمایش لاگ‌های هشدار
+  // debug: console.debug, // لاگ‌های دیباگ را نمایش دهید (در صورت نیاز)
+};
---
+++ src/lib/auth/zod.ts
@@ -0,0 +1,12 @@
+import { object, string } from 'zod';
+
+export const formSchema = object({
+  username: string({ required_error: 'username is requierd' }).min(1).max(20),
+  password: string().min(2).max(20)
+});
+export const formSchema2 = object({
+  username: string({ required_error: 'username is requierd' }).min(1).max(20),
+  password: string().min(2).max(20)
+  .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+  .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+});
