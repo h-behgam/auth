e97189fec6005e81934bedf821930de6a5d8f60c
Hadi Behgam <h.behgam@gmail.com>
2025-01-24 19:20:39 +0330

    new json validation

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,13 +1,22 @@
 'use server';
 
+import { isValidJsonString } from '@/lib/auth/custom-auth-error';
 import { signIn } from '@/lib/auth/next-auth';
 
 export type FormState = {
-  error?: any | undefined;
-  message?: any | undefined;
+  error?: {
+    message: {
+      username?: string;
+      password?: string;
+    } | string | null;
+  }| null;
+  message?: string | undefined;
 };
 
-export const login = async (state: FormState, formData: FormData): Promise<FormState> => {
+export const login = async (
+  state: FormState,
+  formData: FormData,
+): Promise<FormState> => {
   try {
     const { username, password } = Object.fromEntries(formData);
 
@@ -22,12 +31,24 @@ export const login = async (state: FormState, formData: FormData): Promise<FormS
     switch (error.type) {
       case 'CredentialsSignin':
         console.log('aaaa2', { error });
-        // console.log('aaaa2', JSON.parse(error.message).password);
-        return { ...state, message: 'Login failed', error: {...error, message: JSON.parse(error.message)} };
-                
-        default:
-          console.log('aaaa5', { error });
-          return { ...state, message: 'Login failed', error: {...error, message: JSON.parse(error.message)} };
+        return {
+          ...state,
+          message: 'Login failed',
+          error: {
+            ...error,
+            message: isValidJsonString(error.message)
+              ? JSON.parse(error.message)
+              : error.message,
+          },
+        };
+
+      default:
+        console.log('aaaa5', { error });
+        return {
+          ...state,
+          message: 'Login failed',
+          error: { ...error, message: JSON.parse(error.message) },
+        };
     }
   }
 };
