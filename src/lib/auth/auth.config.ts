eceec9e30d0178a6cd4b33ceb72c0ffb61b2af26
Hadi Behgam <h.behgam@gmail.com>
2025-01-25 15:49:26 +0330

    Update auth.config.ts

--- src/lib/auth/auth.config.ts
+++ src/lib/auth/auth.config.ts
@@ -1,29 +1,14 @@
 import Credentials from 'next-auth/providers/credentials';
-// import { PrismaAdapter } from '@auth/prisma-adapter';
 import type { NextAuthConfig } from 'next-auth';
-import { NextResponse } from 'next/server';
+import { redirectAuth } from './auth-helper';
 
-// import PrismaDB from '../PrismaDB';
-
-// import { logger } from './custom-auth-error';
-
-// Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [Credentials],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       // مسیرهای محافظت‌شده را تعریف کنید
-      const loginpage = Response.redirect(new URL('/login', nextUrl));
       const protectedPaths = ['/dashboard'];
       const authRoute = ['/signup', '/login'];
-      //   const isProtected = protectedRoutes.some((route) =>
-      //     request.nextUrl.pathname.startsWith(route)
-      //   );
-
-      //   if (!session && isProtected) {
-      //     const absoluteURL = new URL("/", request.nextUrl.origin);
-      //     return NextResponse.redirect(absoluteURL.toString());
-      //   }
       const isLoggedIn = !!auth?.user;
       console.log(nextUrl.pathname);
 
@@ -38,13 +23,14 @@ export default {
       console.log('isLoggedIn', isLoggedIn);
       console.log('isAuthRoute', isAuthRoute);
 
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isProtected) {
-        if (!isLoggedIn) return Response.redirect(new URL('/login', nextUrl));
+        // if (!isLoggedIn) return Response.redirect(new URL('/login', nextUrl));
+        redirectAuth('/login', nextUrl.origin);
       }
       if (isAuthRoute) {
         if (isLoggedIn)
-          return NextResponse.redirect(new URL('/dashboard', nextUrl));
+          // return NextResponse.redirect(new URL('/dashboard', nextUrl));
+          redirectAuth('/dashboard', nextUrl.origin);
       }
       return true;
     },
