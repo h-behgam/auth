4dbcccd8a4e460050eed1e2b1ecf04a71c593e91
Hadi Behgam <h.behgam@gmail.com>
2025-01-25 17:05:25 +0330

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,9 @@
 import authConfig from '@/lib/auth/auth.config';
 import NextAuth from 'next-auth';
 
-
 // Use only one of the two middleware options below
 // 1. Use middleware directly
-export const { auth: middleware } = NextAuth(authConfig)
-
+export const { auth: middleware } = NextAuth(authConfig);
 
 /**
  * // 2. Wrapped middleware option
@@ -21,8 +19,8 @@ export const { auth: middleware } = NextAuth(authConfig)
 // Middleware configuration
 export const config = {
   // matcher: ['/dashboard', '/signup'],
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/","/(api|trpc)(.*)"],
-  // matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/dashboard"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  // matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
   // matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.svg|.png|.ico).*)"],
   // matcher: ["/((?!api|_next/static|_next/image|.png).*)"],
 };
