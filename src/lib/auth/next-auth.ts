08e062929fe2573a613f6b15e4affcb94d50db0d
Hadi Behgam <h.behgam@gmail.com>
2025-02-03 18:31:23 +0330

    Update next-auth.ts

--- src/lib/auth/next-auth.ts
+++ src/lib/auth/next-auth.ts
@@ -87,12 +87,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   callbacks: {
     async jwt({ account, token, user, profile, session, trigger }) {
-      console.log('account', account);
-      console.log('token', token);
-      console.log('user', user);
-      console.log('profile', profile);
-      console.log('session', session);
-      console.log('trigger', trigger);
+      // console.log('account', account);
+      // console.log('token', token);
+      // console.log('user', user);
+      // console.log('profile', profile);
+      // console.log('session', session);
+      // console.log('trigger', trigger);
       // const sessions = await PrismaAdapter(PrismaDB).createSession!({
       //   userId: user.id!,
       //   sessionToken,
@@ -103,76 +103,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return token;
     },
     async session({ session, token, user }) {
-      console.log('session', session);
-      console.log('token', token);
-      console.log('user', user);
+      // console.log('session', session);
+      // console.log('token', token);
+      // console.log('user', user);
       return session;
     },
   },
 });
-
-// import { PrismaAdapter } from '@next-auth/prisma-adapter';
-// import { NextAuthOptions } from 'next-auth';
-// import PrismaDB from './prisma';
-// import Credentials from 'next-auth/providers/credentials';
-// import { compare } from 'bcrypt';
-// import { User } from '@prisma/client';
-
-// export const authOption: NextAuthOptions = {
-//   adapter: PrismaAdapter(PrismaDB),
-//   secret: process.env.NEXTAUTH_SECRET,
-//   pages: { signIn: '/login' },
-//   session: { strategy: 'jwt' },
-//   debug: process.env.NODE_ENV === 'development',
-//   providers: [
-//     Credentials({
-//       name: 'credential',
-//       credentials: {
-//         username: {
-//           label: 'username',
-//           type: 'string',
-//         },
-//         password: {
-//           label: 'password',
-//           type: 'string',
-//         },
-//       },
-//       async authorize(credential): Promise<any> {
-//         if (!credential?.username || !credential.password) return null;
-
-//         const user = await PrismaDB.user.findUnique({
-//           where: { username: credential.username },
-//         });
-//         if (!user) return null;
-
-//         const isPasswordValid = await compare(
-//           credential.password,
-//           user.hashedPassword as string,
-//         );
-//         if (!isPasswordValid) return null;
-
-//         return user;
-//       },
-//     }),
-//   ],
-//   callbacks: {
-//     session: ({ session }) => {
-//       return {
-//         ...session,
-//         user: {
-//           ...session.user,
-//         },
-//       };
-//     },
-//     jwt: ({token,user}) => {
-//         const u = user as unknown as User
-//         if (u) {
-//             return {
-//                 ...token
-//             }
-//         }
-
-//         return token
-//     }
-//   },
-// };
