de9360e0a121317dd19d92a4543c424f50485a3c
Hadi Behgam <h.behgam@gmail.com>
2025-02-03 18:31:34 +0330

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,11 @@
+'use client';
 import Link from 'next/link';
-import SignoutButton from '@/lib/auth/signout-button';
-import { auth } from '@/lib/auth/next-auth';
+import SignoutButton from '@/components/template/auth/signout-button';
+import { useSession } from 'next-auth/react';
+import { cn } from '@/lib/util';
 
-async function HeaderLayout() {
-  const session = await auth();
+function HeaderLayout() {
+  const { data: session, status } = useSession();
   return (
     <nav className='flex justify-between p-2'>
       <ul className='flex gap-4'>
@@ -40,13 +42,18 @@ async function HeaderLayout() {
           </Link>
         </li>
       </ul>
-      {session?.user ? (
-        <SignoutButton className='w-max rounded-sm bg-slate-100 p-1'>
-          {'Sign Out'}
-        </SignoutButton>
-      ) : (
-        <Link href={'/login'} className='w-max rounded-sm bg-slate-100 p-1'>Sign in</Link>
-      )}
+      <div className={cn('h-10 w-28 rounded-sm bg-slate-100')}>
+        {session?.user && <SignoutButton>{'Sign Out'}</SignoutButton>}
+
+        {!session && status !== 'loading' && (
+          <Link
+            href={'/login'}
+            className='bg-slate-0 block w-full rounded-sm px-5 py-2 text-center text-slate-700 duration-300 hover:bg-slate-50'
+          >
+            Sign in
+          </Link>
+        )}
+      </div>
     </nav>
   );
 }
