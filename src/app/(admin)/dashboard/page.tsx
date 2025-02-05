25d9c0afddfe7dd4548304fa0cdf57ff3c8a8dab
Hadi Behgam <h.behgam@gmail.com>
2025-02-05 17:32:24 +0330

    Update page.tsx

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,12 +1,13 @@
-// import { auth } from '@/lib/auth/next-auth';
-import React from 'react'
+import React from 'react';
 
 async function dashboardPage() {
-  // console.log('session is: ', await auth());
-  
   return (
-    <div>dashboardPage</div>
-  )
+    <main className='min-h-20 py-4'>
+      <div>
+        <p className='text-2xl'>Welcome to Dashboard</p>
+      </div>
+    </main>
+  );
 }
 
-export default dashboardPage
+export default dashboardPage;
