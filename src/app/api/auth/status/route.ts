910b6341eaa0c0c2103a180115599dc736e46f2a
Hadi Behgam <h.behgam@gmail.com>
2025-02-05 17:36:32 +0330

    Delete route.ts

--- src/app/api/auth/status/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(req: Request) {
-    const isAuthenticated = req.headers.get("x-user-authenticated") === "true";
-    return NextResponse.json({ isAuthenticated });
-}
