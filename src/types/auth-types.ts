757fd81fc3925bd6b213410964d7234e3810154d
Hadi Behgam <h.behgam@gmail.com>
2025-02-06 19:56:01 +0330

    Create auth-types.ts

---
+++ src/types/auth-types.ts
@@ -0,0 +1,31 @@
+export type IinputSignup = Record<
+  'username' | 'password' | 'confirmPassword' | 'name' | 'email',
+  string
+>;
+
+export interface Isignup {
+  success: boolean;
+  error: {
+    zod?: Record<string, string|string[]>;
+    other?: string;
+  };
+  data?: {};
+}
+
+export interface IsignupFileds {
+  name: string;
+  username: string;
+  email: string;
+}
+export type LoginFormState = {
+  error?: {
+    message:
+      | {
+          username?: string;
+          password?: string;
+        }
+      | string
+      | null;
+  } | null;
+  message?: string | undefined;
+};
