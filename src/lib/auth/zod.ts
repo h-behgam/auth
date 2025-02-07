c7634893120aad1fe168f2c091fae6becbea04ad
Hadi Behgam <h.behgam@gmail.com>
2025-02-07 22:18:16 +0330

    Update zod.ts

--- src/lib/auth/zod.ts
+++ src/lib/auth/zod.ts
@@ -6,8 +6,13 @@ export const formSchema = object({
 });
 
 export const signupSchema = object({
-  name: string({ required_error: 'Name is requierd' }).min(4).max(40).regex(/[A-Z]/, 'Password must contain at least one uppercase letter'),
-  email: string({ required_error: 'Email is requierd' }).email({message: 'Enter email correctly.'}),
+  name: string({ required_error: 'Name is requierd' })
+    .min(4)
+    .max(40)
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter'),
+  email: string({ required_error: 'Email is requierd' }).email({
+    message: 'Enter email correctly.',
+  }),
   username: string({ required_error: 'Username is requierd' }).min(1).max(20),
   password: string({ required_error: 'Password is requierd' }).min(2).max(20),
   confirmPassword: string({ required_error: 'Please confirm your password' })
