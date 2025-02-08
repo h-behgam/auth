424f2eb1fcafd1d05f73058528f58f99ca7a023f
Hadi Behgam <h.behgam@gmail.com>
2025-02-08 13:08:12 +0330

    Update error-field.tsx

--- src/components/template/auth/error-field.tsx
+++ src/components/template/auth/error-field.tsx
@@ -1,9 +1,16 @@
-function ErrorFiled({ item }: { item: string[] }) {
-  return Object.values(item).map((item) => (
+function ErrorFiled({ item }: { item: string | string[] }) {
+  if (Array.isArray(item)) {
+    return Object.values(item).map((item) => (
+      <p className='mt-1 text-red-700' key={item}>
+        {item}
+      </p>
+    ));
+  }
+  return (
     <p className='mt-1 text-red-700' key={item}>
       {item}
     </p>
-  ));
+  );
 }
 
 export default ErrorFiled;
